944e88e82dc05793fd4fd05e0d600746ecf49198
PunithSFT <punithpc26@gmail.com>
2025-02-25 16:28:57 +0530

    Image

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,29 @@
 import React from "react";
+import CountUp from "react-countup";
+//intersection Observer
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+//variant
+import { fadeIn } from "../variants";
 
 const About = () => {
-  return <div className="">About</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+  return (
+    <div className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div>
+          {/*img*/}
+          <div className="flex-1 bg-services bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top">
+            img
+          </div>
+
+          <div>text</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
